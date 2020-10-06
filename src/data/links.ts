23fa19dc2d8f5fc48d20a5c9e48a4b3c03fcae35
hoonsubin <40356749+hoonsubin@users.noreply.github.com>
2020-10-06 22:45:55 +0900

    changed to gatsby

--- src/data/links.ts
+++
@@ -1,7 +0,0 @@
-export const Links = {
-    twitter: 'https://twitter.com/hoonsubin',
-    github: 'https://github.com/hoonsubin?tab=repositories',
-    homePage: 'https://teamstep.io',
-    facebook: 'https://www.facebook.com/hoonsubin',
-    linkedin: 'https://www.linkedin.com/in/hoon-kim-a95538168/',
-};
